ed61fc42782cb8a6f317966d41b6ca8d2a44eae1
Khushtaunk28 <Khush.taunk@gmail.com>
2025-01-08 22:55:50 +0530

    solved the framer error

--- src/services/Hero.jsx
+++ src/services/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Button from '../components/Button'
 import { arrowRight } from '../assets/icons'
 import { statistics } from '../constants'
-import { div } from 'framer-motion/client'
+
 
 const Hero = () => {
   return (
