91c9f8e7833da2a72d4db0b1afd403dd534f0dc5
Khushtaunk28 <Khush.taunk@gmail.com>
2025-01-14 14:04:37 +0530

    contact us post call is working as on localhost

--- src/sections/ContactUs.jsx
+++ src/sections/ContactUs.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
-import Button from '../components/Button'
 import { toast } from 'react-toastify'
+import axios from 'axios'
 const ContactUs = () => {
 
   const[formData,setFormData] = useState({
@@ -22,7 +22,7 @@ const ContactUs = () => {
   const handleSubmit = async(e) => {
     e.preventDefault();
     try{
-      const response=await axios.post('https://bsa-server.herokuapp.com/contactus',formData,{
+      const response=await axios.post('http://localhost:8080/public/contact-us',formData,{
         headers: {
           'Content-Type': 'application/json'
         }
@@ -42,46 +42,58 @@ const ContactUs = () => {
         </h2>
    <div className='max-w-md flex flex-col gap-4 pt-6 border-2 border-coral-red rounded-s-xl mt-4 p-4 '> 
    <form action='contactus' onSubmit={handleSubmit}>
-    <div class="mb-4">
-      <label for="name" class="block text-lg font-medium text-gray-700">Name:</label>
+    <div className="mb-4">
+      <label htmlFor="name" className="block text-lg font-medium text-gray-700">Name:</label>
       <input
         type="text"
         id="name"
+        name='name'
+        value={formData.name}
+        onChange={handleChange}
         placeholder="Mr xyz" required
-        class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-red-300 focus:border-red-300"
+        className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-red-300 focus:border-red-300"
       />
     </div>
-    <div class="mb-4">
-      <label for="email" class="block text-lg font-medium text-gray-700">Email:</label>
+    <div className="mb-4">
+      <label htmlFor="email" className="block text-lg font-medium text-gray-700">Email:</label>
       <input
         type="email"
         id="email"
+        name='email'
+        value={formData.email}
+        onChange={handleChange}
         placeholder="xyz@bsa.com" required
-        class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-red-300 focus:border-red-300"
+        className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-red-300 focus:border-red-300"
       />
     </div>
-    <div class="mb-4">
-      <label for="phone" class="block text-lg font-medium text-gray-700">Phone:</label>
+    <div className="mb-4">
+      <label htmlFor="phone" className="block text-lg font-medium text-gray-700">Phone:</label>
       <input
         type="tel"
         id="phone" 
+        name='phone'
+        value={formData.phone}
+        onChange={handleChange}
         placeholder="+91XXXXXXXXXX" required
-        class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-red-300 focus:border-red-300"
+        className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-red-300 focus:border-red-300"
       />
     </div>
-    <div class="mb-4">
-      <label for="query" class="block text-lg font-medium text-gray-700">Query:</label>
+    <div className="mb-4">
+      <label htmlFor="query" className="block text-lg font-medium text-gray-700">Query:</label>
       <textarea
         id="query"
         placeholder="Do you sell this size nut-bolt?" required
         rows="4"
-        class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-red-300 focus:border-red-300"
+        name='query'
+        value={formData.query}
+        onChange={handleChange}
+        className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-red-300 focus:border-red-300"
       ></textarea>
     </div>
-    <div class="text-center">
+    <div className="text-center">
       <button
         type="submit"
-        class="px-6 py-2 text-white bg-red-400 rounded-full shadow-lg hover:bg-red-500 hover:scale-105 transform transition-all duration-300"
+        className="px-6 py-2 text-white bg-red-400 rounded-full shadow-lg hover:bg-red-500 hover:scale-105 transform transition-all duration-300"
       >
         Submit
       </button>
