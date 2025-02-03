cf64cec865dbf10ef0fa8f12fad71eb968428dde
Khushtaunk28 <Khush.taunk@gmail.com>
2025-02-03 11:43:58 +0530

    deploying from scratch again

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -53,9 +53,9 @@ const Footer = () => {
         width={20} height={20} className='rounded-full m-0'/>
         <p className='text-white-400 font-montserrat text-base leading-7'>2025 Bihar Supply Agency. All Rights Reserved</p>
       </div>
-      {/* <div>
+      <div>
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3597.884137449571!2d85.13924180985954!3d25.608766014815718!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ed585dcbfe5ccd%3A0xc99658efac2e1f54!2sBihar%20Supply%20Agency!5e0!3m2!1sen!2sin!4v1736418054990!5m2!1sen!2sin" width="400" height="300" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-      </div> */}
+      </div>
       <p className='font-montserrat cursor-pointer'>Terms & Conditions</p>
     </div>
 
